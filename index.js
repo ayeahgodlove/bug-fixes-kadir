102bb355a223785ae0b17c15e6d7dff42123b5d3
Kadirgu <Kadirgu@users.noreply.github.com>
2022-05-10 16:32:50 +0200

    2.5 task
    changes adds

--- index.js
+++ index.js
@@ -1,88 +1,195 @@
-
-const express = require('express'),
-bodyParser = require('body-parser'),
-uuid = require('uuid'),
-morgan = require('morgan');
-
-
+const express = require("express");
+const res = require("express/lib/response");
+      morgan = require('morgan');
+const path = require("path")
 const app = express();
+const PORT = 8080; 
+const uuid = require('uuid');
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const cors = require('cors');
-app.use(cors({}));
-
-let auth = require('./auth')(app);
-
-const passport = require('passport');
-require('./passport');
-
-app.use(morgan('common')); //add morgan middlewar library
-
-
-let movies = [
-{
-Title: "Interstellar",
-Description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-Genre: {
-Name: "Sci-Fi",
-Description: " speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the literature of ideas, and it often explores the potential consequences of scientific, social, and technological innovations."
-},
-
-Director: {
-Name: "Christopher Nolan",
-Bio: "Christopher Nolan is an American director, producer, and screenwriter.",
-Birth: "1970-07-30"
-},
-ImagePath: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRf61mker2o4KH3CbVE7Zw5B1-VogMH8LfZHEaq3UdCMLxARZAB",
-Featured: true
-},
-{
-Title: "The Hobbit",
-Description: "A reluctant Hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home, and the gold within it from the dragon Smaug.",
-Genre: {
-Name: "Fantasy",
-Description: "speculative fiction involving magical elements, typically set in a fictional universe and sometimes inspired by mythology and folklore. Its roots are in oral traditions, which then became fantasy literature and drama. From the twentieth century, it has expanded further into various media, including film, television, graphic novels, manga, animated movies and video games."
-},
-
-Director: {
-Name: "Peter Jackson",
-Bio: "Peter Jackson is an American director, producer, and screenwriter.",
-Birth: "1961-10-31"
-},
-ImagePath: "https://resizing.flixster.com/bvVhpq1XDXo409UQ07ZgFrsIlZ0=/206x305/v2/https://flxt.tmsimg.com/assets/p9458059_p_v8_ac.jpg",
-Featured: true
-},
-{
-Title: "Inception",
-Description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.",
-Genre: {
-Name: "Sci-Fi",
-Description: " speculative fiction which typically deals with imaginative and futuristic concepts such as advanced science and technology, space exploration, time travel, parallel universes, and extraterrestrial life. It has been called the literature of ideas, and it often explores the potential consequences of scientific, social, and technological innovations."
-},
-
-Director: {
-Name: "Christopher Nolan",
-Bio: "Christopher Nolan is an American director, producer, and screenwriter.",
-Birth: "1970-07-30"
-},
-ImagePath: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg",
-Featured: true
+let users = [
+  {
+    id:1,
+    fullname: 'John Doe',
+    email: 'johndoe@mail.com',
+    favMovies: [{
+      title: 'Inception',
+      director: 'Christopher Nolan',
+      genre: 'Sci-Fi'
+    }]
+  },
+  {
+    id:2,
+    fullname: 'Jane Doe',
+    email: 'janedoe@mail.com',
+    favMovies: [{
+      title: 'The Avengers',
+      director: 'Peter Jackson',
+      genre: 'Super-Heroes'
+    }]
+  },
+  {
+  id:3,
+  fullname: 'Iris Lopez',
+  email: 'irislopez@gmail.com',
+  favMovies: [{
+    title: 'Terminator',
+    director: 'James Cameron',
+    genre: 'Action'
+  }]
 }
+
 ];
 
+let movies = [
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan',
+    genre: 'Sci-Fi'
+  },
+  {
+    title: 'Lord of the Rings',
+    director: 'Peter Jackson',
+    genre: 'Super-Heroes'
+  },
+  {
+    title: 'The Matrix',
+    director: 'Lana Wachowski',
+    genre: 'Sci-fi'
+  },
+  {
+      title: 'The Avengers',
+      director: 'Anthony Russo',
+      genre: 'Super-Heroes'
+    },
+    {
+      title: 'The Silence Of The Lambs',
+      director: 'Jonathan Demme',
+      genre: 'Suspense-Thriller'
+    },
+    {
+      title: 'Terminator',
+      director: 'James Cameron',
+      genre: 'Action'
+    },
+    {
+      title: 'The Prestige',
+      director: 'Christopher Nolan',
+      genre: 'Suspense-Thriller'
+    },
+    {
+      title: 'Shutter Island',
+      director: 'Martin Scorsese',
+      genre:'Suspense-Thriller'
+    },
+    {
+      title: 'The Fugitive',
+      director: 'Andrew Davis',
+      genre: 'Suspense-Thriller'
+    },
+    {
+      title: 'The Shack',
+      director: 'Stuart Hazeldine',
+      genre: 'Feel-Good'
+    }
+];
 
-app.use(express.static('public')); //serves “documentation.html” file from the public folder
+// READ to return all movies to user
+app.get('/movies', (req, res) => {
+  res.status(200).json(movies)
+});
 
+//For returning data about a single movie
+app.get('/movies/title/:title', (req, res) => {
+  const movie = movies.find((m)=> m.title == req.params.title);
+  res.send('Request was successful');
+});
+
+//For returning data about a genre
+app.get('/movies/genre/:genre', (req, res) => {
+  const movies_ = movies.filter((m)=> m.genre == req.params.genre);
+  res.send('Request was successful');
+});
+
+//For returning data about a director by name
+app.get('/movies/director/:director', (req, res) => {
+  const director = movies.filter((m)=> m.director == req.params.director);
+  res.send('Request was successful');
+});
+
+//CREATE For allowing new users to register
+app.post('/users/register', (req, res) => {
+  users.push(req.body);
+  res.send('Registration Successful!');
+});
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+//For allowing users to UPDATE their user info
+app.put('/users/update/:id', (req, res) => {
+  let userId =  users.findIndex((u)=>u.id==req.params.id);
+  users.slice(userId,1, {...req.body});
+  res.send('Changes saved successfully!');
+  res.send(users);
+});
+
+//For allowing users to add a movie to their list of favorite movies
+app.post('/favourite/add/:id', (req, res) => {
+  const user = users.find((u) => u.id ==req.params.id);
+  user.favMovies.push(req.body);
+  res.send('Request was successful')
+});
+
+//For allowing users to remove a movie from their list of favorites movies-text
+app.delete('/favourite/delete/:id/:title', (req, res) => {
+  const user = users.find((u) => u.id ==req.params.id);
+  const favs = user.favMovies.filter((m)=>m.title != req.params.title)
+  user.favMovies = [...favs];
+  res.send('Favorite has been removed')
+});
+
+//For allowing existing users to deregister-text
+app.delete('/users/deregister/:id', (req, res) => {
+  users.filter((m) => m.id !=req.params.id);
+  res.send('User details successfully removed!')
+});
+
+//GET request for returning the personal message
+app.get("/", (req, res)=>{
+    res.send("welcome to my flix")
+})
+
+app.get("/documentation", (req, res)=>{
+    res.sendFile(path.join(__dirname,'/public/documentation.html'));
+})
+
+//GET request for returning the JSON movie data
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+//GET request for returning default response
+app.get('/', (req, res) => {
+    res.send('Welcome to the Top 10 Movies List!');
+  });
+
+  //Using the Morgan middleware library to log all requests
+app.use(morgan('common'));
+app.use(express.json()); 
+
+//Using express.static to serve the documentation.html file
+app.use(express.static('public'));
+
+//Error handling middleware
 app.use((err, req, res, next) => {
-console.error(err.stack);
-res.status(500).send('Something broke!');
-});
+    console.error(err.stack);
+    res.status(500).send('Oops!Something Went Wrong!');
+  });
 
-// listen for requests
-const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
-console.log('Listening on Port ' + port);
-});
+//Listen for request
+app.listen(PORT, ()=>console.log("App is running"));
+
+
